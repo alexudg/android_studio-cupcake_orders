fccf553d0238f91a38c275a0b56c05f4255abc65
alexudg <alexudg@gmail.com>
2024-04-30 19:50:38 -0600

    supr import not used

--- app/src/main/java/com/planetsistemas/cupcakeorders/PickUpScreen.kt
+++ app/src/main/java/com/planetsistemas/cupcakeorders/PickUpScreen.kt
@@ -1,6 +1,5 @@
 package com.planetsistemas.cupcakeorders
 
-import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
--- app/src/main/java/com/planetsistemas/cupcakeorders/SummaryScreen.kt
+++ app/src/main/java/com/planetsistemas/cupcakeorders/SummaryScreen.kt
@@ -3,7 +3,6 @@ package com.planetsistemas.cupcakeorders
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -15,7 +14,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 @Composable
 fun SummaryScreen(
